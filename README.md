85436676e5c81bdb6b3231fcda085c560c779fcc
patelria007 <patelria07@gmail.com>
2024-03-02 23:53:00 -0500

    Data preprocessing (part 1)

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # COSC525_ASL_SNN
 UTK COSC525 - Deep Learning class project - Spiking Neural Network for neuromorphic ASL dataset
+
+## Dataset
+Access the converted ASL dataset from `.aedat` (AEDAT 2.0) to `.aedat4` (AEDAT 4.0) here: https://drive.google.com/drive/folders/1eVItyrWI0HDg7sj4hEpfh3w0yUgdz70j?usp=drive_link 
+
+The data is in the form of 5 `.zip` files, one for each of the subjects used in data collection. The original dataset is found here: https://github.com/PIX2NVS/NVS2Graph/tree/master.
+
+### Dataset Characteristics:
+* 24 classes (`A-Y`, excludes `J`)
+* 5 subjects used to pose handshapes
+* 4,200 samples **_per letter_** (100,800 total samples)
+    * Each sample lasts 100 milliseconds 
+* Recorded with iniLabs DAVIS240c NVS camera
+    * Office with constant illumination, low envioronmental noise
+
---
+++ utils/convert_all_to_csv.py
@@ -0,0 +1,43 @@
+import os
+from tqdm import tqdm
+import dv_processing as dv
+
+DATA = "../data"
+CSV = f"{DATA}/csv"
+AEDAT = f"{DATA}/aedat"
+TARBALLS = f"{DATA}/tarballs"
+
+# Create necessary directories
+if not os.path.isdir(DATA):
+    os.mkdir(DATA)
+if not os.path.isdir(TARBALLS):
+    os.mkdir(TARBALLS)
+if not os.path.isdir(CSV):
+    os.mkdir(CSV)
+if not os.path.isdir(AEDAT):
+    os.mkdir(AEDAT)
+
+# Move all zipped files to data
+os.system(F"mv ../*.zip {TARBALLS}")
+
+# Unzip all tarballs
+for i in tqdm(os.listdir(TARBALLS)):
+    if i.endswith(".zip"):
+        FILE = f"{TARBALLS}/{i}"
+        os.system(f"unzip {FILE} -d {AEDAT}")
+        if "__MACOSX" in os.listdir(AEDAT):
+            os.system(f"rm -rf {AEDAT}/__MACOSX")
+
+
+for i in tqdm(sorted(os.listdir(AEDAT))):
+    SUBJECT = f"{AEDAT}/{i}"
+    if not os.path.isdir(SUBJECT):
+        os.mkdir(SUBJECT)
+    
+    EXP = f"{CSV}/{i}"
+    if not os.path.isdir(EXP):
+        os.mkdir(EXP)
+
+    for j in os.listdir(SUBJECT):
+        FILE = f"{SUBJECT}/{j}"
+        os.system(f"python export_aedat4_to_csv.py --file {FILE} --dir {EXP}")
